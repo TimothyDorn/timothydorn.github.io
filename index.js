79451f45f3e68d565353da000a3561aa45e2e9a9
Timothy <timothydorn1927@gmail.com>
2021-08-21 08:13:34 -0500

    create like button

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+'use strict';
+
 
   function Square(props){
     return (
